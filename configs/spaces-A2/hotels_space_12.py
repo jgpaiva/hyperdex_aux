f06b96c87d37a7a459692931a66c0cef3651672c
morazow <m.orazow@gmail.com>
2013-06-13 10:42:54 +0000

    rollback

---
+++ configs/spaces-A2/hotels_space_12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import hyperclient
+import sys
+
+c = hyperclient.Client(str(sys.argv[1]), 1982)
+c.add_space('''
+    space hotels
+    key k
+    attributes string name, string address, float ratings, int stars, 
+               string category, string status, string tel, 
+               string region, string locality, string postcode,
+               string lowest_price, int highest_price, 
+               float longitude, float latitude
+    subspace region
+    subspace postcode
+    subspace address
+    subspace tel
+    tolerate 1 failure
+    ''')
