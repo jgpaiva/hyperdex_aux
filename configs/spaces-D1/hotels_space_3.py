642f3295c9f46e5ea79d078ca3adcf03e320713a
morazow <m.orazow@gmail.com>
2013-07-08 19:48:50 +0000

    add the spaces for workload D

---
+++ configs/spaces-D1/hotels_space_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import hyperclient
+import sys
+
+c = hyperclient.Client(str(sys.argv[1]), 1982)
+c.add_space('''
+    space hotels
+    key k
+    attributes string name, string address, string ratings, string stars, 
+               string category, string status, string tel, 
+               string region, string locality, string postcode,
+               string lowest_price, int highest_price, 
+               float longitude, float latitude
+    subspace region, address, tel, postcode
+    tolerate 1 failure
+    ''')
